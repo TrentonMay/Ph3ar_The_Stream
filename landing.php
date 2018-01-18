ded40d8800c71b50f21d0dd14654d5242e3f8be8
TrentonMay <TrentonMay@fullsail.edu>
2018-01-18 18:59:24 -0500

    Added main php file and assets

---
+++ landing.php
@@ -0,0 +1,28 @@
+/**
+ * Created by PhpStorm.
+ * User: sctrm
+ * Date: 1/18/2018
+ * Time: 6:47 PM
+ */
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css"/>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css"/>
+    <link rel="stylesheet" href="css/main.css">
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<nav>
+    
+</nav>
+
+</body>
+</html>
