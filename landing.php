e17654c4230ab4b286fc981201033a938808dabc
TrentonMay <TrentonMay@fullsail.edu>
2018-01-18 21:49:29 -0500

    Adjusted Navigation

--- landing.php
+++ landing.php
@@ -10,22 +10,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css"/>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css"/>
+    <title>PH3AR The Stream!</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css.map">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.min.css.map">
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
     <link rel="stylesheet" href="css/main.css">
-    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-dark">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <img src="assets/Ph3ar_Logo.png">
-        </div>
-    </div>
-</nav>
+    <nav class="navbar navbar-dark navbar-expand-sm bg-dark fixed-top">
+        <a class="navbar-brand" href="#"><img src="assets/Ph3ar_Logo.png"></a>
+        <ul class="navbar-nav float-right ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Log In</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Streaming</a>
+            </li>
+        </ul>
+
+    </nav>
 
 </body>
 </html>
